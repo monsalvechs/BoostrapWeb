2ed81be3407d0b2341b38fb1401695f099625952
Carlos Monsalve <88389975+monsalvechs@users.noreply.github.com>
2023-07-22 15:27:42 -0300

    Update init.js

--- init.js
+++ init.js
@@ -12,3 +12,18 @@ const cleanUp = () => {
     loader.style.display = "none";
     container.style.display = "block";
 }
+
+window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("btnVolverArriba").style.display = "block";
+      } else {
+        document.getElementById("btnVolverArriba").style.display = "none";
+      }
+    }
+
+    function goToTop() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
